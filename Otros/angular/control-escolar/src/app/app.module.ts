0461f20b42134636207214037ff880e1ae91c40b
Maurice <mauricio.gonzalezt@alumno.buap.mx>
2025-09-18 21:29:43 -0600

    Añadido routing a los layouts (redirección de auth a login)

--- Otros/angular/control-escolar/src/app/app.module.ts
+++ Otros/angular/control-escolar/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuariosComponent } from './screens/registro-usuarios/registro-usuarios.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    RegistroUsuariosComponent,
+    AuthLayoutComponent,
+    DashboardLayoutComponent
   ],
   imports: [
     BrowserModule,
