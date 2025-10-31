091cd38393386cd9ab8a82feee2f7585ac459e36
Maurice <mauricio.gonzalezt@alumno.buap.mx>
2025-10-30 21:49:03 -0600

    Añadido vista de registro

--- Otros/TestSpeed/app-carrera-ducati-frontend/src/app/app-routing.module.ts
+++ Otros/TestSpeed/app-carrera-ducati-frontend/src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroUsuariosComponent } from './screens/registro-usuarios/registro-usuarios.component';
 
 const routes: Routes = [
-  {
-    path: '', // Cuando alguien visite la raíz del sitio...
-    // Carga perezosamente el AuthModule
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  },
-  // Aquí irían otras rutas, por ejemplo, la parte "privada" de la app
-  // {
-  //   path: 'dashboard',
-  //   loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
-  // }
-];
+  { path: 'login', component: LoginScreenComponent },
+  { path: 'registro', component: RegistroUsuariosComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
